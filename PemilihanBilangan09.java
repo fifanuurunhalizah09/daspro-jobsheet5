0dce2e3015660329b3786634b66edbee84367b5d
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-09-26 11:37:36 +0700

    pembilangan

--- PemilihanBilangan09.java
+++ PemilihanBilangan09.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class PemilihanBilangan09 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
-        System.out.print("Masukkan sebuah angka: ");
-        int angka = sc.nextInt();
+        Scanner input = new Scanner(System.in); 
+        int bilangan;
+        String hasil;
 
-        if (angka > 0) {
-            System.out.println("Angka " + angka + " adalah bilangan positif.");
-        } else {
-            System.out.println("Angka " + angka + " adalah bilangan negatif.");
-        }
+        System.out.print("Masukkkan Bilangan: ");
+        bilangan = input.nextInt();
+
+        hasil = (bilangan % 2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
+        System.out.println("Angka: " + bilangan + " adalah " + hasil);
     }
 }
